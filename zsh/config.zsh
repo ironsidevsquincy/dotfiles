461ff09d08a3880d1e4dbfc1b4002a5d5573f979
Darren Hurley <ironsidevsquincy@googlemail.com>
2020-05-16 18:55:11 +0200

    update

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,1 @@
 ZSH_THEME="mine"
-
-plugins=(git osx)
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,4 +1,6 @@
-export PATH=~/bin:$(npm bin):$PATH
+export PATH=~/bin:~/.local/bin:$PATH
 
-export GRUNT_ISDEV=1
-export SLIMERJSLAUNCHER=/Applications/Firefox.app/Contents/MacOS/firefox
+# KLARNA
+export NPM_TOKEN=6af25f87-571e-45d2-9a64-3a3e43a1ce59
+export AD_USERNAME=darren.hurley
+export META_PASS_DIR=/Users/darren.hurley/projects/klarna/meta/meta-pass
