3edc035eb9c4db4b26ad471849e32635b056cdcf
Darren Hurley <ironsidevsquincy@googlemail.com>
2020-05-17 13:29:31 +0200

    remove

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,1 @@
 export PATH=~/bin:~/.local/bin:$PATH
-
-# KLARNA
-export NPM_TOKEN=6af25f87-571e-45d2-9a64-3a3e43a1ce59
-export AD_USERNAME=darren.hurley
-export META_PASS_DIR=/Users/darren.hurley/projects/klarna/meta/meta-pass
